071809b1ef8f215c41ca3d3f264b71ba1e6c9e88
Josh Partlow <joshua.partlow@puppetlabs.com>
2021-09-03 20:50:22 +0000

    (REPLATS-426) Add plan to teardown replicated apps
    Deletes the app resources, optionally removes app entry from
    admin-console.
    
    Deleting kotsadm itself is a todo item.

---
+++ plans/teardown.pp
@@ -0,0 +1,60 @@
+# In successive tiers, teardown the application, it's admin-console
+# metadata, and the Kots admin-console itself, if desired.
+#
+# By default, just deletes the given app's resources, allowing
+# for a fresh deployment.
+#
+# @param kots_slug
+#   The slug label of the Replicated application to delete.
+# @param kots_namespace
+#   The k8s namespace the application is installed in.
+# @param scaledown_timeout
+#   The number of seconds to wait for the application to scale down while
+#   deleting resources.
+# @param remove_app_from_console
+#   If true, then the application metadata will also be deleted from the
+#   admin-console. The next installation via `kubectl-kots install` will
+#   recreate a fresh entry for the application in the console.
+# @param delete_kotsadm
+#   If tue, then the Kotsadm admin-console will also be deleted from
+#   the cluster. It should be reinstalled by the next `kubectl-kots install`
+#   command.
+plan pam_tools::teardown(
+  TargetSpec $targets,
+  String $kots_slug,
+  String $kots_namespace = 'default',
+  Integer $scaledown_timeout = 300,
+  Boolean $remove_app_from_console = false,
+  Boolean $delete_kotsadm = false,
+) {
+
+  $destroy_app_results = run_task('pam_tools::delete_k8s_app_resources', $targets, {
+    'kots_slug'         => $kots_slug,
+    'kots_namespace'    => $kots_namespace,
+    'scaledown_timeout' => $scaledown_timeout,
+  })
+
+  if $remove_app_from_console {
+    $remove_app_from_console_results = run_task('pam_tools::delete_kots_app', $targets, {
+      'kots_slug'      => $kots_slug,
+      'kots_namespace' => $kots_namespace,
+      'force'          => true,
+    })
+  } else {
+    $remove_app_from_console_results = 'not-done'
+  }
+
+  if $delete_kotsadm {
+    $delete_kotsadm_results = 'todo'
+  } else {
+    $delete_kotsadm_results = 'not-done'
+  }
+
+  $results = {
+    'kots_slug'                          => $kots_slug,
+    'destroy_app_result_set'             => $destroy_app_results,
+    'remove_app_from_console_result_set' => $remove_app_from_console_results,
+    'delete_kotsadm_result_set'             => $delete_kotsadm_results,
+  }
+  return $results
+}
---
+++ spec/plans/teardown_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'pam_tools::teardown' do
+  # Include the BoltSpec library functions
+  include BoltSpec::Plans
+
+  let(:targets) { ['spec-host'] }
+  let(:params) do
+    {
+      'targets'   => targets,
+      'kots_slug' => 'app',
+    }
+  end
+
+  before(:each) do
+    expect_task('pam_tools::delete_k8s_app_resources')
+      .with_targets(targets)
+      .with_params(
+        'kots_slug'         => 'app',
+        'kots_namespace'    => 'default',
+        'scaledown_timeout' => 300,
+      )
+  end
+
+  it 'deletes app' do
+    result = run_plan('pam_tools::teardown', params)
+
+    expect(result.ok?).to eq(true)
+    expect(result.value['kots_slug']).to eq('app')
+    expect(result.value['destroy_app_result_set']).to be_kind_of(Bolt::ResultSet)
+    expect(result.value['remove_app_from_console_result_set']).to eq('not-done')
+    expect(result.value['delete_kotsadm_result_set']).to eq('not-done')
+  end
+
+  it 'deletes and removes app' do
+    params['remove_app_from_console'] = true
+    expect_task('pam_tools::delete_kots_app')
+      .with_targets(targets)
+      .with_params(
+        'kots_slug'      => 'app',
+        'kots_namespace' => 'default',
+        'force'          => true,
+      )
+
+    result = run_plan('pam_tools::teardown', params)
+
+    expect(result.ok?).to eq(true)
+    expect(result.value['kots_slug']).to eq('app')
+    expect(result.value['destroy_app_result_set']).to be_kind_of(Bolt::ResultSet)
+    expect(result.value['remove_app_from_console_result_set']).to be_kind_of(Bolt::ResultSet)
+    expect(result.value['delete_kotsadm_result_set']).to eq('not-done')
+  end
+
+  it 'deletes and removes app and deletes kotsadm'
+end
