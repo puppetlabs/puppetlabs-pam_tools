2e2a4d4cfe41fc26bb7932cecabb0bcfe9c263f5
Josh Partlow <joshua.partlow@puppetlabs.com>
2021-10-13 16:56:20 -0700

    (REPLATS-457) Return loadbalancer ip when starting ingress
    After starting the nginx ingress, poll for the loadbalancer ip and return
    it in the task data so it can be used by the caller.
    
    Useful so that you know the ip to test your application on after
    installation, and for setting up hostname/certs when using dev work
    arounds like nip.io addresses that embed the ip to short circuit lack of
    dns for ephemeral test instances.
    
    This commit also includes two brain dead fixes for the start/destroy
    tasks in that they previously did not run themselves when executed.

--- spec/tasks/start_nginx_ingress_spec.rb
+++ spec/tasks/start_nginx_ingress_spec.rb
@@ -5,21 +5,24 @@ require_relative '../../tasks/start_nginx_ingress.rb'
 
 describe 'pam_tools::start_nginx_ingress' do
   let(:task) { StartNginxIngress.new }
+  let(:timeout) { 10 }
   let(:args) do
     {
       version: '0.0.0',
       provider: 'spec',
-      timeout: '1',
+      timeout: timeout,
     }
   end
   let(:source) { 'https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.0.0/deploy/static/provider/spec/deploy.yaml' }
-
-  it 'runs' do
-    apply_command = [
+  let(:apply_command) do
+    [
       'kubectl',
       'apply',
       "--filename=#{source}",
     ]
+  end
+
+  before(:each) do
     expect(task).to(
       receive(:run_command)
         .with(apply_command)
@@ -31,10 +34,24 @@ describe 'pam_tools::start_nginx_ingress' do
           include(
             'kubectl',
             'rollout',
-            '--timeout=1s',
+            "--timeout=#{timeout}s",
           )
         )
         .and_return('waited')
+    )
+  end
+
+  it 'runs' do
+    expect(task).to(
+      receive(:run_command)
+        .with(
+          include(
+            'kubectl',
+            'get',
+            'svc/ingress-nginx-controller',
+          )
+        )
+        .and_return('10.20.30.40')
     )
 
     expect(task.task(args)).to(
@@ -43,8 +60,59 @@ describe 'pam_tools::start_nginx_ingress' do
           command: apply_command.join(' '),
           apply_output: 'applied',
           wait_output: 'waited',
+          load_balancer_ip: '10.20.30.40',
         }
       )
     )
+  end
+
+  context 'when getting lb ip' do
+    let(:timeout) { 1 }
+    let(:retries) { 1 }
+
+    before(:each) do
+      expect(task).to(
+        receive(:run_command)
+          .with(
+            include(
+              'kubectl',
+              'get',
+              'svc/ingress-nginx-controller',
+            )
+          )
+          .and_return('')
+          .exactly(retries).times
+      )
+    end
+
+    it 'retries ip lookup' do
+      expect(task).to(
+        receive(:run_command)
+          .with(
+            include(
+              'kubectl',
+              'get',
+              'svc/ingress-nginx-controller',
+            )
+          )
+          .and_return('10.20.30.40')
+      )
+
+      expect(task.task(args)).to(
+        match(
+          include(
+            load_balancer_ip: '10.20.30.40',
+          )
+        )
+      )
+    end
+
+    context 'times out' do
+      let(:retries) { 2 }
+
+      it 'raises an error if timeout exceeded' do
+        expect { task.task(args) }.to raise_error(%r{No loadbalancer IP})
+      end
+    end
   end
 end
--- tasks/start_nginx_ingress.rb
+++ tasks/start_nginx_ingress.rb
@@ -14,7 +14,8 @@ class StartNginxIngress < PAMTaskHelper
     ]
     apply_output = run_command(apply_command)
 
-    # ingress object creation hard fails until the controller is ready
+    # Any ingress object creation hard fails until the controller is
+    # eady, so wait.
     wait_command = [
       'kubectl',
       'rollout',
@@ -26,10 +27,39 @@ class StartNginxIngress < PAMTaskHelper
     ]
     wait_output = run_command(wait_command)
 
+    # Get the ingress load balancer ip to return to the caller so they
+    # have some idea of what to hit to test their app connection.
+    lb_ip_lookup_command = [
+      'kubectl',
+      'get',
+      'svc/ingress-nginx-controller',
+      '--namespace=ingress-nginx',
+      %(--output=jsonpath={.status.loadBalancer.ingress[0].ip}),
+    ]
+    load_balancer_ip = ''
+    counter = 0
+    # There may still be a delay before the ip address is readable
+    while load_balancer_ip.empty?
+      load_balancer_ip = run_command(lb_ip_lookup_command)
+      break if !load_balancer_ip.empty?
+
+      counter += 1
+      if counter > timeout.to_i
+        raise TaskHelper::Error.new(
+          "No loadbalancer IP address retrieved after #{timeout} seconds of polling.",
+          'pam_tools/ingress-nginx-no-loadbalancer-ip'
+        )
+      end
+      sleep 1
+    end
+
     {
       command: apply_command.join(' '),
       apply_output: apply_output,
       wait_output: wait_output,
+      load_balancer_ip: load_balancer_ip,
     }
   end
 end
+
+StartNginxIngress.run if __FILE__ == $PROGRAM_NAME
